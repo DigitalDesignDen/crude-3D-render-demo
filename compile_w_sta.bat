85eeff4d4114acb350787245e6b9c6d9ec97005f
DigitalDesignDen <maister2677960@gmail.com>
2026-01-15 23:13:00 +0100

    fixed typos in compile scripts

--- compile_w_sta.bat
+++ compile_w_sta.bat
@@ -1,3 +1,4 @@
 G:\altera\13.0sp1\quartus\bin64\quartus_sh --flow compile graphics_pipe.qpf -c graphics_pipe_top
 
-G:\altera\13.0sp1\quartus\bin64\quartus_pgm -c USB-Blaster .\output_files\graphics_pipe_top.cdf
+G:\altera\13.0sp1\quartus\bin64\quartus_pgm -c USB-Blaster -o "p;.\output_files\graphics_pipe_top.sof" -m JTAG
+REM G:\altera\13.0sp1\quartus\bin64\quartus_pgm -c USB-Blaster .\output_files\graphics_pipe_top.cdf
